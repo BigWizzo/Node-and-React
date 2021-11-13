f8945db74d892c3fee6988a1ddd136685cebdbfa
willnyamunokora <wnnwillard@gmail.com>
2021-11-13 10:52:18 +0200

    Initialize server with apollo and express

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at port 4000`));
+}
+
+startApolloServer(typeDefs, resolvers);
