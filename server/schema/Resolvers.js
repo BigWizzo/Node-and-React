942cd4c80b9262ec669111ee68c28f20a36779ce
willnyamunokora <wnnwillard@gmail.com>
2021-11-13 13:47:23 +0200

    Put axios requests in services requests file

--- server/schema/Resolvers.js
+++ server/schema/Resolvers.js
@@ -1,43 +1,20 @@
-const axios = require('axios');
-
-const baseURL = 'https://swapi.dev/api/people/';
+const { requests } = require('../services/requests');
 
 const resolvers = {
   Query: {
-    getAllPeople: async () => {
-      return axios.get(baseURL).then((res) => res.data);
-    },
-
-    getPeopleByName: async (_, { name }, { Person }) => {
-      return axios.get(baseURL + `?search=${name}`).then((res) => res.data);
-    },
-    getPersonById: async (_, { id }) => {
-      return axios.get(baseURL + `${id}`).then((res) => res.data);
-    },
-
-    getAllPeoplePage: async (_, { id }) => {
-      return axios.get(baseURL + `?page=${id}`).then((res) => res.data);
-    },
-
-    getPeopleByNamePage: async (_, { name, id }) => {
-      return axios
-        .get(baseURL + `?search=${name}&page=${id}`)
-        .then((res) => res.data);
-    },
+    getAllPeople: requests.getAllPeople,
+    getPeopleByName: requests.getPeopleByName,
+    getPersonById: requests.getPersonById,
+    getAllPeoplePage: requests.getAllPeoplePage,
+    getPeopleByNamePage: requests.getPeopleByNamePage,
   },
 
   Person: {
-    homeworld: async (parent) => {
-      return axios.get(parent.homeworld).then((res) => res.data);
-    },
+    homeworld: requests.getHomeworld,
   },
 
   People: {
-    results: async (parent) => {
-      return parent.results.map((person) => {
-        return axios.get(person.url).then((res) => res.data);
-      });
-    },
+    results: requests.getResults,
   },
 };
 
---
+++ server/services/requests.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+const baseURL = 'https://swapi.dev/api/people/';
+
+const requests = {
+  getAllPeople: async () => {
+    return axios.get(baseURL).then((res) => res.data);
+  },
+
+  getPeopleByName: async (_, { name }) => {
+    return axios.get(baseURL + `?search=${name}`).then((res) => res.data);
+  },
+  getPersonById: async (_, { id }) => {
+    return axios.get(baseURL + `${id}`).then((res) => res.data);
+  },
+
+  getAllPeoplePage: async (_, { id }) => {
+    return axios.get(baseURL + `?page=${id}`).then((res) => res.data);
+  },
+
+  getPeopleByNamePage: async (_, { name, id }) => {
+    return axios
+      .get(baseURL + `?search=${name}&page=${id}`)
+      .then((res) => res.data);
+  },
+
+  getHomeworld: async (parent) => {
+    return axios.get(parent.homeworld).then((res) => res.data);
+  },
+
+  getResults: async (parent) => {
+    return parent.results.map((person) => {
+      return axios.get(person.url).then((res) => res.data);
+    });
+  },
+};
+
+module.exports = { requests };
